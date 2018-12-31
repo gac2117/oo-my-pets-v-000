05b5fe77741c197c5817fe88639720ab1d934567
Grace Choi <oiograceoio@gmail.com>
2018-12-31 07:17:04 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,8 +1,12 @@
 class Owner
   @@all = {fishes: [], cats: [], dogs: []} # code goes here
 
-  attr_accessor :pets
+  attr_accessor :pets, :owner
 
+  def initialize(owner) 
+    @owner = owner
+  end
+  
   def self.all
     @@all
   end
