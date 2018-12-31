5c3e470ae535bb3419c4bfd11c4c4a57c32c469d
Grace Choi <oiograceoio@gmail.com>
2018-12-31 07:39:48 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -5,13 +5,13 @@ class Owner
   attr_reader :species
 
   def initialize(species)
-    @species = species 
-    OWNERS << self 
+    @species = species
+    OWNERS << self
     @pets = {fishes: [], cats: [], dogs: []}
   end
-  
+
   def self.all
-    OWNERS 
+    OWNERS
   end
 
   def self.count
